3dee455c79f78047e8b3311afc65002c6b85d9f2
Mosesotieno-ai <mosesotieno8363@gmail.com>
2025-07-21 22:14:40 +0300

    refactor: Remove axios integration and simplify API service functions

--- src/services/api.js
+++ src/services/api.js
@@ -1,39 +1,16 @@
-import axios from "./axiosInstance";
-
-/**
- * Generic GET request
- * @param {string} url
- * @param {object} config
- */
 export function get(url, config = {}) {
-  return axios.get(url, config).then((res) => res.data);
+  // Return mock data or empty object
+  return Promise.resolve({});
 }
 
-/**
- * Generic POST request
- * @param {string} url
- * @param {object} data
- * @param {object} config
- */
 export function post(url, data = {}, config = {}) {
-  return axios.post(url, data, config).then((res) => res.data);
+  return Promise.resolve({});
 }
 
-/**
- * Generic PUT request
- * @param {string} url
- * @param {object} data
- * @param {object} config
- */
 export function put(url, data = {}, config = {}) {
-  return axios.put(url, data, config).then((res) => res.data);
+  return Promise.resolve({});
 }
 
-/**
- * Generic DELETE request
- * @param {string} url
- * @param {object} config
- */
 export function del(url, config = {}) {
-  return axios.delete(url, config).then((res) => res.data);
+  return Promise.resolve({});
 }
